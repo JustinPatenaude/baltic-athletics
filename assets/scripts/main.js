6eca10e26db7ab1ce75a048944cf224717562403
Justin Patenaude <justinpatenaude92@gmail.com>
2019-03-25 20:19:38 +0000

    Deploy from Forestry.io

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,43 +16,43 @@ $(document).ready(function(){
   /* ==========================
      RESPONSIVE MENU
   ========================== */
-  if($(window).width() > 768){
-    $(window).resize(function(){
-      responsiveNav();
-    });
-    responsiveNav();
-  } else {
-    $('.nav__item-wrapper .nav__item').prependTo('.sub-nav__inner').removeClass('nav__item').addClass('sub-nav__item');
-  }
+  // if($(window).width() > 768){
+  //   $(window).resize(function(){
+  //     responsiveNav();
+  //   });
+  //   responsiveNav();
+  // } else {
+  //   $('.nav__item-wrapper .nav__item').prependTo('.sub-nav__inner').removeClass('nav__item').addClass('sub-nav__item');
+  // }
 
   /* ==========================
      RESPONSIVE MENU
   ========================== */
-  $('.category__search-form .search__input').on('keyup', function(e){
-    var inputtedValue = e.target.value;
-    $('.catalogue__category-title').each(function(){
-      var title = $(this).text().toLowerCase();
-      if(title.indexOf(inputtedValue) < 0){
-        $(this).closest('.catalogue__category-wrapper').hide();
-      } else {
-        $(this).closest('.catalogue__category-wrapper').show();
-      }
-      if($(this).closest('.category').height() < 81){
-        $(this).closest('.category').hide();
-      } else {
-        $(this).closest('.category').show();
-      }
-    });
-  });
+  // $('.category__search-form .search__input').on('keyup', function(e){
+  //   var inputtedValue = e.target.value;
+  //   $('.catalogue__category-title').each(function(){
+  //     var title = $(this).text().toLowerCase();
+  //     if(title.indexOf(inputtedValue) < 0){
+  //       $(this).closest('.catalogue__category-wrapper').hide();
+  //     } else {
+  //       $(this).closest('.catalogue__category-wrapper').show();
+  //     }
+  //     if($(this).closest('.category').height() < 81){
+  //       $(this).closest('.category').hide();
+  //     } else {
+  //       $(this).closest('.category').show();
+  //     }
+  //   });
+  // });
 });
 
-function responsiveNav(){
-  var maxWidth = $('.nav__wrapper').outerWidth(),
-      navItemWrapper = $('.nav__item-wrapper').outerWidth(),
-      navItemButton = $('.nav__item--has-sub').outerWidth(),
-      total = navItemWrapper+navItemButton+30;
-  if(total > maxWidth){
-    $('.nav__item-wrapper .nav__item').last().prependTo('.sub-nav__inner').removeClass('nav__item').addClass('sub-nav__item');
-    responsiveNav();
-  }
-}
+// function responsiveNav(){
+//   var maxWidth = $('.nav__wrapper').outerWidth(),
+//       navItemWrapper = $('.nav__item-wrapper').outerWidth(),
+//       navItemButton = $('.nav__item--has-sub').outerWidth(),
+//       total = navItemWrapper+navItemButton+30;
+//   if(total > maxWidth){
+//     $('.nav__item-wrapper .nav__item').last().prependTo('.sub-nav__inner').removeClass('nav__item').addClass('sub-nav__item');
+//     responsiveNav();
+//   }
+// }
