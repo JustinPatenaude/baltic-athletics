3e13add2f953f6a4105671c25987cbd7f548d7fc
Justin Patenaude <justinpatenaude92@gmail.com>
2019-06-11 02:05:48 +0000

    Deploy from Forestry.io

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -28,34 +28,6 @@ $(document).ready(function(){
   });
 
   /* ==========================
-     MASONRY
-  ========================== */
-  // var macyColumns = 1;
-  // var macyContainer = '#suppliers';
-  // if($('#suppliers-2').length){
-  //   console.log('suppliers 2');
-  //   macyColumns = 2;
-  //   macyContainer = '#suppliers-2';
-  // } else if($('#suppliers-3').length){
-  //   console.log('suppliers 3');
-  //   macyColumns = 3;
-  //   macyContainer = '#suppliers-3';
-  // }
-  // console.log('macyColumns', macyColumns);
-  // var macyInstance = Macy({
-  //   container: macyContainer,
-  //   trueOrder: false,
-  //   waitForImages: false,
-  //   columns: macyColumns,
-  //   margin: 20
-  // });
-
-  // macyInstance.on(macyInstance.constants.EVENT_IMAGE_COMPLETE, function (ctx) {
-  //   $('.suppliers__all').removeClass('fade-out').addClass('fade-in');
-  //   $('.suppliers__loading').removeClass('fade-in').addClass('fade-out');
-  // });
-
-  /* ==========================
      CATALOGUE SEARCH
   ========================== */
   $('.category__search-form .search__input').on('keyup', function(e){
@@ -67,7 +39,6 @@ $(document).ready(function(){
       } else {
         $(this).closest('.catalogue__category-wrapper').show();
       }
-      // console.log($(this).text(), $(this).closest('.category').height());
       if($(this).closest('.category').height() < 81){
         $(this).closest('.category').hide();
       } else {
@@ -92,6 +63,15 @@ $(document).ready(function(){
     });
   });
 
+  /* ==========================
+     CATEGORY PHOTOS
+  ========================== */
+  $('.category__photo-wrapper, .gallery__photo-wrapper').magnificPopup({
+    type: 'image',
+    gallery:{
+      enabled: true
+    }
+  });
 
 
   /* ==========================
